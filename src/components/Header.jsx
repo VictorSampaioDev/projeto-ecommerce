5f15ec1eb24789d34128665a514d0895a53e8571
Visitante Aldeota <visitante@digitalcollege.internal>
2026-02-24 11:45:05 -0300

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 const Header = () => {
     return (
-        <div>
-            <p className="font-mono  p-2 rounded-xl border-2 bg-purple-800">header</p>
-            <p className="font-mono  p-2 rounded-xl border-2 bg-purple-700">header</p>
-        </div>
-
+        <header>
+            <div>
+                header
+            </div>
+        </header>
     );
 }
 
