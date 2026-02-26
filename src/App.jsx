9ac47d292436131739eec2ac3b9b9e00272ee5d7
unknown <lucassilvino0772@digitalcollege.internal>
2026-02-26 09:21:05 -0300

    Criando header e menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import Paths from "./routes/Paths";
-
-const App = () => {
-  return ( 
-    <Paths/>
-   );
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      
+      <main className="p-6">
+        <h1 className="text-2xl font-bold">
+          Conteúdo da página
+        </h1>
+      </main>
+    </>
+  );
 }
- 
+
 export default App;
