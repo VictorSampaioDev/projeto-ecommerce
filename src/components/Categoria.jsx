a332c879b719d2c64ae1640c795348dcccce8268
igor0344 <igorandradelira@hotmail.com>
2026-02-26 12:04:47 -0300

    atualização

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,12 +1,17 @@
 
 const Categoria = () => {
-    return (
-
-<div>
-    Categoria
-</div>
-       
-    );
+  return (
+    <div className="card">
+      <img 
+        src="https://via.placeholder.com/300x150" 
+        alt="Imagem do card" 
+      />
+      <h2>Título do Card</h2>
+      <p>Este é um exemplo simples de um card feito apenas com HTML e CSS.</p>
+      <button>Saiba mais</button>
+    </div>
+  );
 }
 
 export default Categoria;
+
